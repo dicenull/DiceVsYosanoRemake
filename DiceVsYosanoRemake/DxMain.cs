613f583ba4401a5c40de706315114156d40bb629
s1500729 <s1500729@sendai-nct.jp>
2019-03-18 19:45:11 +0900

    プロジェクト ファイルを追加します。

---
+++ DiceVsYosanoRemake/DxMain.cs
@@ -0,0 +1,25 @@
+﻿using DxLibDLL;
+using DxLibUtilities;
+
+namespace DXLibWrapper
+{
+    public class DxMain
+    {
+        static void Main(string[] args)
+        {
+            DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+
+            Window.IsWindowMode = true;
+
+            var status = DX.DxLib_Init();
+            if (status == -1)
+            {
+                return;
+            }
+
+            new Program().Run();
+
+            DX.DxLib_End();
+        }
+    }
+}
---
+++ DiceVsYosanoRemake/Program.cs
@@ -0,0 +1,16 @@
+﻿using DxLibUtilities;
+using DxLibDLL;
+using DxLogic;
+using Diagram;
+using Utilities;
+
+public class Program
+{
+    public void Run()
+    {
+        while (DxSystem.Update())
+        {
+            new Circle(Input.Mouse.Point, 10).Draw(Palette.Blue);
+        }
+    }
+}
