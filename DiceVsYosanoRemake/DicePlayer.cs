5dce29eb911830896790e78024527196cff576df
s1500729 <s1500729@sendai-nct.jp>
2019-03-20 12:12:26 +0900

    プレイヤークラスを追加

---
+++ DiceVsYosanoRemake/DicePlayer.cs
@@ -0,0 +1,51 @@
+﻿using Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace DiceVsYosanoRemake
+{
+    public class DicePlayer
+    {
+        private List<Texture> diceList;
+        private Texture damagedDice;
+
+        private Texture statusTexture;
+
+        public int Speed { get; set; } = 2;
+
+        public Rectangle PlayerArea { get; set; }
+
+        public void Move(Direction dir)
+        {
+            PlayerArea.TopLeft += dir.ToVector() * Speed;
+
+            statusTexture = diceList[(int)dir + 1];
+        }
+
+        public void Draw()
+        {
+            statusTexture.Scaled(new Vector2D(PlayerArea.Size.w, PlayerArea.Size.h));
+
+            statusTexture.Draw(PlayerArea.TopLeft);
+
+            statusTexture = diceList[0];
+        }
+        
+        public DicePlayer(Rectangle area, IEnumerable<Texture> diceList)
+        {
+            this.diceList = diceList.ToList();
+            this.damagedDice = diceList.Last();
+
+            this.diceList.Remove(damagedDice);
+
+            PlayerArea = area;
+
+            // 状態を初期化
+            statusTexture = this.diceList[0];
+        }
+    }
+}
---
+++ DiceVsYosanoRemake/Direction.cs
@@ -0,0 +1,26 @@
+﻿using Utilities;
+
+namespace DiceVsYosanoRemake
+{
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+
+    public static class DirectionExtend
+    {
+        public static Vector2D ToVector(this Direction dir)
+        {
+            return dir switch
+            {
+                Direction.Up => new Vector2D(0, -1),
+                Direction.Right => new Vector2D(1, 0),
+                Direction.Down => new Vector2D(0, 1),
+                Direction.Left => new Vector2D(-1, 0)
+            };
+        }
+    }
+}
